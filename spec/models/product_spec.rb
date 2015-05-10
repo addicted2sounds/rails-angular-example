57c3b6f61caecb0a6dcd4c6b1deb5944fd5eef1b
addicted <addicted22sounds@gmail.com>
2015-05-10 05:56:25 +0300

    product factory spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'factory' do
+    it 'is valid' do
+      expect(build :product).to be be_valid
+    end
+  end
 end
