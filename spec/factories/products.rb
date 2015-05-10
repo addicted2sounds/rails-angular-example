940783e8e0157efdbc6bc35822f221925f3fe370
addicted <addicted22sounds@gmail.com>
2015-05-10 05:59:49 +0300

    product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :product do
-    
+    name { Faker::Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph }
+    price Faker::Commerce.price
   end
 
 end
