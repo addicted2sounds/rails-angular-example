830c23aa7e077fd4931dcedd207fc621537908f3
addicted <addicted22sounds@gmail.com>
2015-05-10 16:04:34 +0300

    alias to product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509083439) do
+ActiveRecord::Schema.define(version: 20150510130303) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150509083439) do
     t.string   "price_currency", default: "USD", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "alias"
   end
 
   create_table "users", force: true do |t|
