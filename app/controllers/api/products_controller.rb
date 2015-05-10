176a13dd2e0f8a396574ea3d369644cbc9c3aed5
addicted <addicted22sounds@gmail.com>
2015-05-10 07:31:00 +0300

    updated product controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,7 +1,22 @@
 class Api::ProductsController < Api::ApplicationController
   respond_to :json
+  before_action :set_product, only: [:show]
+
   def index
     @products = Product.all
-    respond_with @products
+  end
+
+  def show
+  end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
   end
 end
